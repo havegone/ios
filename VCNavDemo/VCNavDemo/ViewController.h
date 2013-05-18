74d5158e750fb46a6775c0ad89adfc5e6f41f898
Tommy <whf727@outlook.com>
2013-05-18 17:12:40 +0800

    upload some useful projects

---
+++ VCNavDemo/VCNavDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  VCNavDemo
+//
+//  Created by OranWu on 13-1-6.
+//  Copyright (c) 2013年 Oran Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *dddd;
+
+@end
